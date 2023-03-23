ba3c7e3023801c68567ab9e2dacbe92266e38999
kumilachew-g <kumilachew1212@gmail.com>
2023-03-23 11:52:31 +0300

    Add routes for the page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ArticlePage from "./pages/ArticlePage";
+import ArticlesListPage from "./pages/ArticlesListPage";
 
 function App() {
   return (
@@ -9,6 +12,10 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/articles-list" element={<ArticlesListPage />} />
+          <Route path="/article/:name" element={<ArticlePage />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
         </Routes>
       </div>
     </Router>
