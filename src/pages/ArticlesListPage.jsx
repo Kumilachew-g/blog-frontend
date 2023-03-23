3fd81b210a64fa4ff9f0bbaf22eba4e015f5e7bc
kumilachew-g <kumilachew1212@gmail.com>
2023-03-23 18:01:15 +0300

    Refactor ArticlesListPage func

--- src/pages/ArticlesListPage.jsx
+++ src/pages/ArticlesListPage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import articles from "./ArticleContent";
 
 const ArticlesListPage = () => (
   <>
     <h1>Articles</h1>
-    <p>Here are some articles</p>
+    {articles.map((article, key) => (
+      <Link
+        className="article-list-item"
+        key={key}
+        to={`/article/${article.name}`}
+      >
+        <h3>{article.title}</h3>
+        <p>{article.content[0].substring(0, 150)}...</p>
+      </Link>
+    ))}
   </>
 );
 
