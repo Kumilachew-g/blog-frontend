0a091a77fa45756b772a950d62e4a9eac93b75ba
kumilachew-g <kumilachew1212@gmail.com>
2023-03-24 10:44:55 +0300

    Refactor ArticlePage func

--- src/pages/ArticlePage.jsx
+++ src/pages/ArticlePage.jsx
@@ -5,15 +5,15 @@ import ArticlesList from "../components/ArticlesList";
 
 const ArticlePage = () => {
   const { name } = useParams();
-  const article = articles.find((article) => article.name === name);
+  const matchingArticle = articles.find((article) => article.name === name);
 
-  if (!article) return <h1>Article does not exist!</h1>;
+  if (!matchingArticle) return <h1>Article does not exist!</h1>;
 
   const otherArticles = articles.filter((article) => article.name !== name);
   return (
     <>
-      <h1>{article.title}</h1>
-      {article.content.map((paragraph, key) => (
+      <h1>{matchingArticle.title}</h1>
+      {matchingArticle.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
 
