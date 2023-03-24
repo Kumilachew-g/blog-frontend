a74cb5e4cca08da273a691e0277fcb298f0ba845
kumilachew-g <kumilachew1212@gmail.com>
2023-03-24 10:44:20 +0300

    Fix bugs

--- src/components/ArticlesList.jsx
+++ src/components/ArticlesList.jsx
@@ -9,7 +9,7 @@ const ArticlesList = ({ articles }) => {
           <Link
             className="article-list-item"
             key={key}
-            to={`/article/${article.name}`}
+            to={`/articles/${article.name}`}
           >
             <h3>{article.title}</h3>
             <p>{article.content[0].substring(0, 150)}...</p>
